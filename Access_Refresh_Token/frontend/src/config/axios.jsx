efa95eaf0c0a6e942a1f8b1a57bb92cb310fce25
Anikate Kanoo <anikate955@gmail.com>
2026-07-25 14:42:15 +0530

    Final changes

--- Access_Refresh_Token/frontend/src/config/axios.jsx
+++ Access_Refresh_Token/frontend/src/config/axios.jsx
@@ -9,10 +9,10 @@ axiosInstance.interceptors.response.use(
     async (error) => {
         const originalRequest = error.config;
 
-        if (error.response?.status === 401 || !originalRequest._retry) {
+        if (error.response?.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true;
             try {
-                await axios.get("http://localhost:3000/api/auth/get-accessToken", {
+                await axiosInstance.get("http://localhost:3000/api/auth/get-accessToken", {
                     withCredentials: true,
                 });
                 return axiosInstance(originalRequest);
@@ -20,6 +20,7 @@ axiosInstance.interceptors.response.use(
                 if (window.location.pathname !== "/") {
                     window.location.href = "/";
                 }
+                // Interveptor ka kaam khatam ho gaya, ab error ko apni jagah bej do
                 return Promise.reject(err);
             }
         }
